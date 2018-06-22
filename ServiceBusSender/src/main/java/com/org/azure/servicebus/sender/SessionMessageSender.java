824adf7a71ef4027bbcad299378ed021fe5b0708
Sandip Patil <sanppatil@gmail.com>
2018-06-22 16:57:36 +0530

    Added some changes

--- ServiceBusSender/src/main/java/com/org/azure/servicebus/sender/SessionMessageSender.java
+++ ServiceBusSender/src/main/java/com/org/azure/servicebus/sender/SessionMessageSender.java
@@ -6,28 +6,39 @@ import com.microsoft.azure.servicebus.*;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 
+public class SessionMessageSender {
+	private static final String connectionString = "xxx";
+	private static final String entityName = "xxx";
+	private static final boolean usingQueue = false;
+	private static final int totalMsgCount = 10;
 
-public class SessionMessageSender
-{
-	private static final String connectionString = "secrete_connectionstring_key";
-	private static final String queueName = "secrete_queue_name";
-	
-    public static void main( String[] args ) throws InterruptedException, ServiceBusException 
-    {
-    	
-		ConnectionStringBuilder csb = new ConnectionStringBuilder(connectionString, queueName);
+	public static void main(String[] args) throws InterruptedException, ServiceBusException {
+
+		ConnectionStringBuilder csb = new ConnectionStringBuilder(connectionString, entityName);
 		csb.setOperationTimeout(Duration.ofSeconds(10));
-		QueueClient queueClient = new QueueClient(csb, ReceiveMode.PEEKLOCK);
-		 
-	    int flightCount=3;
-        for (int i=0; i<10; i++)
-        {
-            int iModValue=i%flightCount;
-            Message message = new Message("Test message " + i +" Session-"+iModValue);
-            message.setSessionId("Session"+iModValue);
-            queueClient.sendAsync(message);
-        }
-        
-        System.out.println("Message published successfully");
-    }
+		if (usingQueue) {
+			QueueClient queueClient = new QueueClient(csb, ReceiveMode.PEEKLOCK);
+
+			int flightCount = 3;
+			for (int i = 0; i < totalMsgCount; i++) {
+				int iModValue = i % flightCount;
+				Message message = new Message("Test message " + i + " Session-" + iModValue);
+				message.setSessionId("Session" + iModValue);
+				queueClient.sendAsync(message);
+			}
+		} else
+		{
+			TopicClient topicClient = new TopicClient(csb);
+
+			int flightCount = 3;
+			for (int i = 0; i < totalMsgCount; i++) {
+				int iModValue = i % flightCount;
+				Message message = new Message("Test message " + i + " Session-" + iModValue);
+				message.setSessionId("Session" + iModValue);
+				topicClient.sendAsync(message);
+			}
+		}
+		
+		System.out.println("Message published successfully");
+	}
 }
