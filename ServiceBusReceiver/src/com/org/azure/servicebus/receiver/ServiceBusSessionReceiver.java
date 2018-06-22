cd77f8b8b07d8c6427c58759f8ca51077eca9270
Sandip Patil <sanppatil@gmail.com>
2018-06-22 16:56:47 +0530

    Added some changes

--- ServiceBusReceiver/src/com/org/azure/servicebus/receiver/ServiceBusSessionReceiver.java
+++ ServiceBusReceiver/src/com/org/azure/servicebus/receiver/ServiceBusSessionReceiver.java
@@ -11,59 +11,72 @@ import com.microsoft.azure.servicebus.*;
 import com.microsoft.azure.servicebus.primitives.ConnectionStringBuilder;
 import com.microsoft.azure.servicebus.primitives.ServiceBusException;
 
-public class ServiceBusSessionReceiver 
-{
+public class ServiceBusSessionReceiver {
 
-    private static final String connectionString = "secrete_connectionstring_key";
-    private static final String queueName = "secrete_queue_name";
-    
-	public static void main(String[] args) throws InterruptedException, ServiceBusException, IOException 
-	{
-		System.out.println("Press any key to sop receiver ...");
+	private static final String connectionString = "xxx";
+	private static String entityName = "xxx";
+	private static final boolean usingQueue = false;
 
-		// create client
-		ConnectionStringBuilder csb = new ConnectionStringBuilder(connectionString, queueName);
-		csb.setOperationTimeout(Duration.ofSeconds(5));
-		QueueClient queueClient = new QueueClient(csb, ReceiveMode.PEEKLOCK);
-		
+	public static void main(String[] args) throws InterruptedException, ServiceBusException, IOException {
+		System.out.println("Press any key to stop receiver ...");
+
 		// configure session handler
-		int maxSession=1;
-		boolean autoComplete=false;
-		Duration dur=Duration.ofSeconds(300);
-		queueClient.registerSessionHandler(new SessionHandler(),new SessionHandlerOptions(maxSession, autoComplete, dur));
-		System.out.println("Timestamp: " + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()) + " *** Session Handler Registered...");
-		
+		if (usingQueue) {
+			ConnectionStringBuilder csb = new ConnectionStringBuilder(connectionString, entityName);
+			csb.setOperationTimeout(Duration.ofSeconds(300));
+			QueueClient queueClient = new QueueClient(csb, ReceiveMode.PEEKLOCK);
+
+			queueClient.registerSessionHandler(new SessionHandler(),
+					new SessionHandlerOptions(1, 1, false, Duration.ofSeconds(300), Duration.ofSeconds(20)));
+
+		} else {
+			entityName = entityName + "//subscriptions//DomainEvents";
+
+			ConnectionStringBuilder csb = new ConnectionStringBuilder(connectionString, entityName);
+			csb.setOperationTimeout(Duration.ofSeconds(300));
+			SubscriptionClient subscriptionClient = new SubscriptionClient(csb, ReceiveMode.PEEKLOCK);
+
+			subscriptionClient.registerSessionHandler(new SessionHandler(),
+					new SessionHandlerOptions(3, 1, false, Duration.ofSeconds(300), Duration.ofSeconds(20)));
+
+		}
+
+		System.out.println("Timestamp: " + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date())
+				+ " *** Session Handler Registered...");
+
 		System.in.read();
-		System.out.println("Closing queue client.");
-		queueClient.close();
 	}
-	
-	static class SessionHandler implements ISessionHandler 
-	{
 
-        @Override
-	    	public CompletableFuture<Void> onMessageAsync(IMessageSession session, IMessage message) 
-	    	{
-	    		String messageString = new String(message.getBody(), StandardCharsets.UTF_8);
-	    		System.out.println("Received message: " + messageString + " for SessionId: " + message.getSessionId());
-	    		return session.completeAsync(message.getLockToken());
-	
-	    	}
-	
-	    	@Override
-	    	public CompletableFuture<Void> OnCloseSessionAsync(IMessageSession session) 
-	    	{
-	    		System.out.println("Timestamp: " + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date()) + " *** Session closed - " + session.getSessionId() + "\n");
-	    		return session.closeAsync();
-	
-	    	}
-	    	
-        @Override
-        public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
-        		System.out.println(exceptionPhase + "-" + throwable.getMessage());
-        }
-        
+	static class SessionHandler implements ISessionHandler {
 
-    }
+		@Override
+		public CompletableFuture<Void> onMessageAsync(IMessageSession session, IMessage message) {
+			try {
+				Thread.sleep(6000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			String messageString = new String(message.getBody(), StandardCharsets.UTF_8);
+			System.out.println("Timestamp: " + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date())
+					+ " Processing completed for message: " + messageString + " for SessionId: " + message.getSessionId());
+			return session.completeAsync(message.getLockToken());
+
+		}
+
+		@Override
+		public CompletableFuture<Void> OnCloseSessionAsync(IMessageSession session) {
+			System.out.println("Timestamp: " + new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date())
+					+ " *** Session closed - " + session.getSessionId() + "\n");
+			return CompletableFuture.completedFuture(null);
+
+		}
+
+		@Override
+		public void notifyException(Throwable throwable, ExceptionPhase exceptionPhase) {
+			System.out.println(exceptionPhase + "-" + throwable.getMessage());
+		}
+
+	}
 
 }
